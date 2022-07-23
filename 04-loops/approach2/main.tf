658da56f247643c857ce819a32054fc82b011893
vishnu <vishnu.akidi@gmail.com>
2022-07-24 01:15:09 +0530

    autocommit

---
+++ 04-loops/approach2/main.tf
@@ -0,0 +1,4 @@
+module "ec2" {
+  source = "./module"
+  count  = 2
+}
---
+++ 04-loops/approach2/module/main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "web" {
+   ami           = "ami-096bf0fa7bc26ab09"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform"
+  }
+}
