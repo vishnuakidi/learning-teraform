1c382182944b3899a789b8b97ddab4d34d8f3e21
vishnu <vishnu.akidi@gmail.com>
2022-07-23 19:44:19 +0530

    autocommit

--- 02-vars/main.tf
+++ 02-vars/main.tf
@@ -6,8 +6,8 @@ output "a"{
   value = var.a
 }
 
-output "x"{
-  value = "Value of a = ${var.a}"
+output "values"{
+  value = "Value of a = ${var.a}, b = ${var.b}, c = ${var.c} d = ${var.d}"
 }
 
 variable "b" {}
